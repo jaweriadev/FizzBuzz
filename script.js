4686f8a29b28128f5854e570be39025558ea288d
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2023-04-12 12:20:12 +0500

    updated

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ var output = [];
 var count = 1;
 
 function FizzBuzz() {
-  if (count % 3 == 0 && count % 5 == 0) {
-    output.push("FizzBuzz");
-  } else if (count % 3 === 0) {
-    output.push("Fizz");
-  } else if (count % 5 === 0) {
-    output.push("buzz");
-  } else {
-    output.push(count);
+  while (count <= 100) {
+    if (count % 3 == 0 && count % 5 == 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
   }
-  count++;
   console.log(output);
 }
